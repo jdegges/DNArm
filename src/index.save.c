c42cc909bcbe73adb73f3f3d204d69a9f90788e4
Joey Degges <jdegges@gmail.com>
2010-05-16 18:22:00 -0700

    Move processing into the read loop and account for the trailing 4 bytes at the end of every process loop.

--- src/index.save.c
+++ src/index.save.c
@@ -16,35 +16,48 @@ int main(int argc, char **argv){
 
 	uint32_t *input = (uint32_t*) malloc(INPUTLEN*sizeof(uint32_t));
 
-	for(;;){	
-		int numRead = fread(input, sizeof(uint32_t), INPUTLEN, fp);
+  // read the first int from the file
+	uint32_t last = 0;
+	int numRead = fread (&last, sizeof *last, 1, fp);
+	if (!numRead){
+		// Not even 4 bytes in the file, exit
+		return 1;
+	}
+
+	for(;;){
+    // the first slot of input will be the last int read in the previous loop
+		int numRead = fread(input + 1, sizeof(uint32_t), INPUTLEN - 1, fp);
 		if (!numRead){
 			// Done, exit
 			return 1;
-		}    
-	}
-
-
-	int i;
-
-	for (i = 0; i < INPUTLEN - 1; i++){
-		uint32_t next = input[i+1];
-		uint64_t whole = input[i];
-   
-		//printf("%llu\n", whole);
-		whole = (whole << 32) | next;
-		printf("%llu\n", whole);
-		//printf("%llu\n", whole);
-   
-		int j;
-		uint64_t temp;
-		for (j = 0; j < KEYLEN; j++){
-			// shift "whole" left by j places, then shift result right 32 places
-			temp = (whole << j) >> 32;
-			uint32_t key = temp;
-			//printf("key:%llu\n", key);
-			//INSERT INTO DB
-			//db_insert(struct db *d, key, KEYLEN);
 		}
+
+		input[0] = last;
+
+		int i;
+
+		for (i = 0; i < INPUTLEN - 1; i++){
+			uint32_t next = input[i+1];
+			uint64_t whole = input[i];
+
+			//printf("%llu\n", whole);
+			whole = (whole << 32) | next;
+			printf("%llu\n", whole);
+			//printf("%llu\n", whole);
+		
+			int j;
+			uint64_t temp;
+			for (j = 0; j < KEYLEN; j++){
+				// shift "whole" left by j places, then shift result right 32 places
+				temp = (whole << j) >> 32;
+				uint32_t key = temp;
+				//printf("key:%llu\n", key);
+				//INSERT INTO DB
+				//db_insert(struct db *d, key, KEYLEN);
+			}
+		}
+
+    // save the last int since it hasnt been processed yet
+		last = input[i];
 	}
 }
